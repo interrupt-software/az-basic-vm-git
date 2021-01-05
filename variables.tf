4479ab7e5a2077e687daee066ac2e7d04a71e3ea
gcastill0 <gilberto@hashicorp.com>
2021-01-05 11:41:56 -0500

    Basic files

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "prefix" {
+  type    = string
+  default = "interrupt-demo01"
+}
+
+variable "location" {
+  type    = string
+  default = "Canada East"
+}
+
+variable "tfadmin" {
+  type    = string
+  default = "tfadmin"
+}
